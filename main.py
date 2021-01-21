c422796be0e2d73bcd0f4c80d6a2419ae296b94f
Tim232 <junseongi284@gmail.com>
2021-01-22 00:01:03 +0900

    reload will help testing codes

--- main.py
+++ main.py
@@ -32,4 +32,21 @@ for file in glob.glob("cogs/*.py"):
 async def on_ready():
 	print("Ready!", bot.user.name, bot.user.id)
 
+@bot.command()
+@commands.is_owner()
+async def reload(ctx, ext=None):
+	if ext is not None:
+		try:
+			bot.reload_extension('cogs.'+ext)
+		except:
+			await ctx.send(f"An error occured while reloading {ext}\n```py\n{traceback.format_exc()}\n```")
+	else:
+		for file in glob.glob("cogs/*.py"):
+			fname = file.replace(os.sep, '.')[:-3]
+			try:
+				bot.reload_extension(fname)
+				await ctx.send(f"Found and reloaded {fname}")
+			except:
+				await ctx.send(f"An error occured while reloading {fname}\n```py\n{traceback.format_exc()}\n```")
+		
 bot.run(config.token)
